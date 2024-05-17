41265ecee2af9380fda931e539836cac6024d902
Jessy Johnson <jessy@pop-os.localdomain>
2024-05-17 15:11:08 -0400

    Initial commit to Github repo

---
+++ microservice.py
@@ -0,0 +1,64 @@
+# Source: https://www.geeksforgeeks.org/matplotlib-pyplot-plot_date-function-in-python/amp/
+
+# importing libraries
+
+import matplotlib.pyplot as plt
+import datetime
+from datetime import timedelta
+import json
+import zmq
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    received = socket.recv()
+    received = received.decode("utf-8")
+    json_received = json.loads(received)
+    print(f"This is json_received {json_received}")
+    print(f"This is received {received}")
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    for key in json_received:
+        print(f"This is the key: {key}")
+        print(f"This is the value: {json_received[key]}")
+        if key == "start":
+            start_date = json_received[key]
+        if key == "end":
+            end_date = json_received[key]
+        if key == "assets":
+            print("This is assets")
+
+    # Finding all dates between two dates
+    start_string = start_date
+    end_string = end_date
+    start_date = datetime.datetime.strptime(start_string, '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(end_string, '%Y-%m-%d').date()
+
+    # Finding all dates between two dates
+    delta = end_date - start_date
+
+    date_array = []
+    for i in range(delta.days + 1):
+        day = start_date + timedelta(days=i)
+        print(day)
+        date_array.append(day)
+    print(date_array)
+
+    # Array of dates for x axis
+    dates = date_array
+
+    # for y axis
+    x = [0, 1, 2, 3, 4]
+
+    plt.plot(dates, x, 'g')
+    plt.xticks(rotation=70)
+    plt.show()
+
+    #  Send reply back to client
+    socket.send(b"Success")
---
+++ ui.py
@@ -0,0 +1,20 @@
+import json
+import zmq
+
+context = zmq.Context()
+#msg = "This will be a JSON object"
+
+#  Socket to talk to server
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+example_json = '{"start":"2000-10-01", "end":"2000-10-05", "assets": ["1.0", "2.0", "4.0", "3.5", "5.5"]}'
+#data = json.dumps(example_json)
+
+while True:
+    #socket.send(bytes(data, 'utf-8'))
+    socket.send(bytes(example_json, 'utf-8'))
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply [ %s ]" % (message))
